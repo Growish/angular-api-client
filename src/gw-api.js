1a09c3312911fc576858ca9105a67b1a92c764d6
Gaia94 <riccardigaia94@gmail.com>
2018-04-18 14:37:50 +0200

    New method AdminGreeting

--- src/gw-api.js
+++ src/gw-api.js
@@ -391,6 +391,8 @@ angular.module('gwApiClient', ['ngCookies']).service('gwApi', function ( $q, $ht
 
     methods.add('deleteListWallpaper', '/image/{0}/');
 
+    methods.add('adminGreeting', '/admin-greeting/{0}/');
+
     var RequestClass = function (method, args) {
 
         var me = this;
